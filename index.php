57fc7e8e564616b4cc488440530043a47be2a0b6
Vitz15 <vitojonathan1515@gmail.com>
2026-04-12 15:26:58 +0800

    minor

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $total_petani = $q_petani['total'];
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>PantauPanen - Sulawesi Barat</title>
+<title>PantauPanen - Sulawesi Utara</title>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;900&family=DM+Sans:wght@300;400;500&display=swap" rel="stylesheet">
